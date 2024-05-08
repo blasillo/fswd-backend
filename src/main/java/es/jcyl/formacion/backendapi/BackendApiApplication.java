8e21c3bfbec15d53bdc92b55ecb16f3c4c65593d
btorregrosa <btorregrosa@infor.uva.es>
2024-05-08 20:38:46 +0200

    JPA entidades con entidadbase

--- src/main/java/es/jcyl/formacion/backendapi/BackendApiApplication.java
+++ src/main/java/es/jcyl/formacion/backendapi/BackendApiApplication.java
@@ -8,10 +8,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BackendApiApplication {
 
     public static void main(String[] args) {

c994ec8eebcc6a51e7683e5241d746967deeef7b
unknown <blasillous@gmail.com>
2024-05-11 18:56:42 +0200

    validacion excepcion

--- src/main/java/es/jcyl/formacion/backendapi/controladores/GestorExcepcion.java
+++ src/main/java/es/jcyl/formacion/backendapi/controladores/GestorExcepcion.java
@@ -4,9 +4,13 @@ package es.jcyl.formacion.backendapi.controladores;
 import es.jcyl.formacion.backendapi.excepciones.RespuestaExcepcion;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
@@ -15,12 +19,30 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 @RestControllerAdvice
 public class GestorExcepcion {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<RespuestaExcepcion> gestorNoValido (MethodArgumentNotValidException exp) {
+
+        Set<String> errores = new HashSet<>();
+        exp.getBindingResult().getAllErrors()
+                .forEach(error -> {
+                    var errorMessage = error.getDefaultMessage();
+                    errores.add(errorMessage);
+                });
+        return ResponseEntity
+                .status(BAD_REQUEST)
+                .body(
+                        RespuestaExcepcion.builder()
+                                .erroresValidation( errores )
+                                .build()
+                );
+    }
+
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<RespuestaExcepcion> gestor (Exception exp) {
+    public ResponseEntity<RespuestaExcepcion> gestorExcepcion (Exception exp) {
         exp.printStackTrace();
 
-        return  ResponseEntity.status( BAD_REQUEST )
+        return  ResponseEntity.status( INTERNAL_SERVER_ERROR )
                 .body(
                         RespuestaExcepcion.builder()
                                 .codigoError(404)
--- src/main/java/es/jcyl/formacion/backendapi/modelos/TareaModelo.java
+++ src/main/java/es/jcyl/formacion/backendapi/modelos/TareaModelo.java
@@ -12,20 +12,20 @@ public class TareaModelo {
 
     private Integer id;
 
-    @NotNull (message = "100")
-    @NotEmpty (message = "100")
+    @NotNull (message = "El nombre no puede estar vacío")
+    @NotEmpty (message = "El nombre no puede estar vacío")
     @Size (max = 200)
     private String  nombre;
 
-    @Min(0)
-    @Max(100)
+    @Min(value = 0, message = "El estado no puede ser menor que 0")
+    @Max(value=100, message = "El estado no puede ser mayor que 100")
     private Integer estado;
 
     @Size (max = 50)
     private String  color;
 
     @Email
-    @NotNull
-    @NotEmpty
+    @NotNull (message = "El correo es obligatorio")
+    @NotEmpty (message = "El correo es obligatorio")
     private String  usuarioCorreo;
 }

8feb337845f4c391f9a377428ac3ccee849ec7d3
unknown <blasillous@gmail.com>
2024-05-12 22:04:36 +0200

    cors

---
+++ src/main/java/es/jcyl/formacion/backendapi/configuracion/WebConfig.java
@@ -0,0 +1,13 @@
+package es.jcyl.formacion.backendapi.configuracion;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+}

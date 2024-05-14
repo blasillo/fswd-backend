83ca7836d4a97d118cf2cfda7f6ef0cf668890c5
btorregrosa <btorregrosa@infor.uva.es>
2024-05-14 09:12:21 +0200

    seguridad

--- src/main/java/es/jcyl/formacion/backendapi/BackendApiApplication.java
+++ src/main/java/es/jcyl/formacion/backendapi/BackendApiApplication.java
@@ -16,7 +16,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditinggit
 public class BackendApiApplication {
 
     public static void main(String[] args) {

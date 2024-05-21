28a02f90baa78927e7c684365e67827c712ebb44
Blas <blasillous@gmail.com>
2024-05-21 23:33:57 +0200

    lab 6

--- src/main/java/es/jcyl/formacion/backendapi/controladores/TareaControlador.java
+++ src/main/java/es/jcyl/formacion/backendapi/controladores/TareaControlador.java
@@ -11,23 +11,27 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("tareas")
+@RequestMapping // TODO: poner el recuros tareas
 @RequiredArgsConstructor
 public class TareaControlador {
 
-    private final TareaServicio servicio;
+    //TODO inyectar el servicio: TareaServicio servicio;
 
 
     @PostMapping
     public ResponseEntity<TareaModelo>  nuevaTarea (
-            @Valid @RequestBody TareaModelo modelo) {
-        return ResponseEntity.ok(  servicio.crearTarea ( modelo) );
+            // TODO: recoger del body un obejto valido TareaModelo modelo
+            ) {
+        return ResponseEntity.ok(  // TODO : llamar al servicio para crear nueva tarea
+                 );
     }
 
     @GetMapping
     public ResponseEntity<List<TareaModelo>> listadoTareas (
-            @RequestParam("correo") String correo) {
-        return ResponseEntity.ok ( servicio.obtenerTareas(correo));
+            //TODO recoger el parametro correo: String correo
+            ) {
+        return ResponseEntity.ok ( // TODO : llamar al servicio para recuperar datos
+                                    );
     }
 
     @PutMapping()

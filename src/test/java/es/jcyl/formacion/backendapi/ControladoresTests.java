dcac393b00a077347f06bdfb3f001d4bdb0dfc10
btorregrosa <btorregrosa@infor.uva.es>
2024-05-22 19:31:56 +0200

    tests controdadores mockmvc

---
+++ src/test/java/es/jcyl/formacion/backendapi/ControladoresTests.java
@@ -0,0 +1,91 @@
+package es.jcyl.formacion.backendapi;
+
+import es.jcyl.formacion.backendapi.persistencia.entidades.Usuario;
+import es.jcyl.formacion.backendapi.persistencia.repositorios.TareasRepositorio;
+import es.jcyl.formacion.backendapi.persistencia.repositorios.UsuariosRepositorio;
+import es.jcyl.formacion.backendapi.servicios.TareaMapeo;
+import es.jcyl.formacion.backendapi.servicios.TareaServicio;
+import es.jcyl.formacion.backendapi.servicios.TareaServicioImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+public class ControladoresTests {
+
+    @Autowired
+    private UsuariosRepositorio usuariosRepo;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Usuario usuarioTest;
+
+    @BeforeEach
+    void setUp() {
+
+        if ( usuariosRepo.findByCorreo("fede.valverde@fmail.com").isEmpty()) {
+            Usuario usuario = Usuario.builder()
+                    .nombreCompleto("Fede Valverde")
+                    .iniciales("FV")
+                    .correo("fede.valverde@fmail.com")
+                    .clave("Password1234#")
+                    .build();
+            usuarioTest = usuariosRepo.save(usuario);
+        }
+    }
+
+    @Test
+    public void testNuevaTarea() throws Exception {
+        String requestBody = """
+                {
+                        "nombre": "Demo",
+                        "estado": 0,
+                        "color": "red-500",
+                        "usuarioCorreo": "fede.valverde@fmail.com"
+                }
+                """;
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/tareas")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.nombre").value("Demo"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNotEmpty() );
+    }
+
+
+    @Test
+    public void testListadoTareas() throws Exception {
+
+        String correo = "fede.valverde@fmail.com";
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/tareas")
+                        .param("correo", correo)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isArray())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].nombre").exists());
+    }
+
+
+
+
+
+
+
+
+}
